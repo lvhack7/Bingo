a62498eea7a58c542be2004c44d20d68f18a9154
Haknazar117 <67150813+Haknazar117@users.noreply.github.com>
2021-07-16 12:07:29 +0600

    Update SidebarOption.js

--- components/SidebarOption.js
+++ components/SidebarOption.js
@@ -1,7 +1,7 @@
 function SidebarOption({Icon, title, selected}) {
     return (
         <div className={`flex items-center space-x-2 pb-3 border-b-2 border-transparent
-            hover:text-red-500 hover:border-red-500 cursor-pointer`}>
+            hover:text-red-500 hover:border-red-500 cursor-pointer ${selected && "text-red-500 border-red-500"}`}>
             <Icon className="h-6"/>
             <p >{title}</p>
         </div>
