4ba9bcc2bfeedcae9b41d8d5dd75ffcd2e085611
Haknazar117 <67150813+Haknazar117@users.noreply.github.com>
2021-09-13 09:59:20 +0600

    Update firebase.js

--- pages/firebase.js
+++ pages/firebase.js
@@ -11,12 +11,7 @@ import 'firebase/auth'
 } = process.env;*/
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC_afHQ3HFoeXXnivZQnz8YStiIMjY_kwg",
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    //Api info
 };
 
 if(!firebase.apps.length) {
